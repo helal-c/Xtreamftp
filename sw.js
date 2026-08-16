b7a0059b212ead6e56f8714db0c0ddc50ddfc7ee
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-16 20:36:02 +0600

    up

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', function(event) {
-    console.log('Xtream TV Service Worker Installed');
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', function(event) {
-    // ব্রাউজারকে ইন্সটল পপআপ দেখানোর জন্য এই ফেচ ইভেন্টটি থাকা বাধ্যতামূলক
-});
+self.addEventListener('fetch', function(event) {});
